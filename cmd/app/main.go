baa0729be01c70a509d844410aed176a298cdb53
bankk <suriya.yim1150@gmail.com>
2025-02-04 20:40:39 +0700

    install package + connect db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,30 @@
 package main
 
-/* import (
-	"fmt"
+import (
+	"go_income_outflow/databases"
 	"log"
-	"net/http"
-) */
+	"os"
 
-const port = 8080
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+var Conn *gorm.DB
 
 func main() {
+	if os.Getenv("APP_ENV") != "production" {
+		if err := godotenv.Load("../../.env"); err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
 
+	databases.ConnectDB()
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+
+	r := gin.Default()
+	r.Use(cors.New(corsConfig))
 }
--- databases/database.go
+++ databases/database.go
@@ -1,1 +1,51 @@
 package databases
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	Conn *gorm.DB
+	once sync.Once
+)
+
+func ConnectDB() {
+	once.Do(func() {
+		dsn := fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s search_path=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_SSL_MODE"),
+			os.Getenv("DB_SCHEMA"),
+		)
+
+		db, err := gorm.Open(
+			postgres.Open(dsn),
+			&gorm.Config{Logger: logger.Default.LogMode(logger.Info)},
+		)
+		if err != nil {
+			log.Fatal("Cannot connect to the database")
+		}
+
+		Conn = db
+	})
+}
+
+/* func Migrate() {
+	Conn.AutoMigrate(
+		&model.Category{},
+		&model.Product{},
+		&model.Order{},
+		&model.OrderItem{},
+	)
+} */
